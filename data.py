794c83770cd895b3171107997fb58c8101c1e09c
Neo-Nguyen-95 <dzung.usth@gmail.com>
2025-04-05 16:23:29 +0700

    update project

--- data.py
+++ data.py
@@ -5,7 +5,23 @@ from bs4 import BeautifulSoup
 
 from nltk import ngrams
 from collections import Counter
+import tiktoken
+encoding = tiktoken.encoding_for_model("text-embedding-3-small")
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+from openai import OpenAI
+
+from pyvi import ViTokenizer
+from gensim import corpora
+from gensim.models import LdaModel
+import re
+import string
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #%% CRAWLING
 class URLCrawl:
@@ -60,6 +76,12 @@ class DataRepository:
         
         return df
     
+    def load_setiment_docs(self):
+        df = pd.read_csv("database/content_wt_sentiment.csv")
+        df.drop(columns='Unnamed: 0', inplace=True)
+        
+        return df
+    
     def export_ngram_list(self, num_gram=2, num_top=20):
         text=" ".join(self.load_documents()['content'])
        
@@ -83,6 +105,186 @@ class DataRepository:
         df = pd.DataFrame(result)
         # df.to_excel(f'{num_top} most common {num_gram}-gram words.xlsx')
         return df
+    
+    def count_tokens(self, doc):
+        return len(encoding.encode(doc))
+    
+    def remove_long_docs(self):
+        max_tokens = 8192
+        
+        df = self.load_documents()
+        
+        df['token_count'] = df['content'].apply(lambda x: self.count_tokens(x))
+        
+        df_filtered = df[df['token_count'] < max_tokens]
+              
+        print(f"Remove {len(df)-len(df_filtered)} overly long docs!")
+        
+        return df_filtered
+        
+    
+    def classify_gpt(self, csv_file_name):
+        api_key = os.getenv("SECRETE_KEY")
+        
+        def classify_text(text):
+            labels = [
+                "Yếu tố kinh tế vĩ mô (bao gồm: lạm phát, lãi xuất, tăng trưởng, chính sách tiền tệ/tài khoá)",
+                "Yếu tố tiền tệ và tài chính (bao gồm: USD index, tỷ giá ngoại tệ, cung tiền, nợ công, lợi suất trái phiếu)",
+                "Yếu tố địa chính trị và xã hội (bao gồm: khủng hoảng địa chính trị, chiến tranh, thiên tai, đại dịch)",
+                "Yếu tố thị trường tài chính (bao gồm: biến động thị trường chứng khoán, đầu cơ, khủng hoảng tài chính)",
+                "Yếu tố khác"
+                ]
+            
+            messages = [
+                {"role": "system", 
+                 "content": "Bạn là một mô hình phân loại văn bản viết về vàng."},
+                {"role": "user", 
+                 "content": f"""Cho văn bản sau: \" {text} \"
+                Danh sách nhãn: {', '.join(labels)}
+                Chỉ trả về một nhãn duy nhất nêu lên tác động chính tới giá vàng. 
+                Không thêm kí tự nào.
+                """}
+                ]
+                 
+            client = OpenAI(api_key = api_key)
+            
+            response = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=messages,
+                temperature=0
+                )
+            
+            return response.choices[0].message.content
+        
+        def classify_documents(documents):
+            values = []
+            
+            for text in documents:
+                topic = classify_text(text)
+                values.append(topic)
+            
+            return values
+        
+        df = self.remove_long_docs()
+
+        df['sentiment'] = classify_documents(df['content'])
+        
+        df.to_csv(csv_file_name)
+        
+        return print('Successful classification!')
+    
+    def grouping_lda(self):
+
+        ### Stop word
+        with open("database/vietnamese-stopwords.txt", "r") as file:
+            vn_stopwords = file.read().splitlines()
+        
+        vn_stopwords = [word.replace(' ', '_') for word in vn_stopwords]
+                
+        ### Tokenize   
+        tokens = [  
+            [word.lower() for word in ViTokenizer.tokenize(doc).split()
+             if word.lower() not in vn_stopwords
+             ] for doc in self.load_documents()['content']
+            ]
+        
+        def keep_meaningful_word(word):
+            for num_str in '1234567890':
+                if num_str in word:
+                    return False
+            
+            if re.fullmatch(r'\W+', word):
+                return False
+            
+            if all(char in string.punctuation for char in word):
+                return False
+            
+            return True
+        
+        tokens_updated = [
+            [w for w in token if keep_meaningful_word(w)]
+            for token in tokens
+            ]
+        
+        all_words = []
+        for token in tokens_updated:
+            all_words.extend(token)
+        
+        counter_word = Counter(all_words)
+        top_common_word = [
+            word[0] for word in counter_word.most_common(int(0.05*len(counter_word)))
+            ]
+        top_10k_word = [
+            word[0] for word in counter_word.most_common(
+                int(0.05*len(counter_word)) + 10000
+                )
+            ]
+        
+        (pd.Series(top_common_word[:100])
+         .rename('top_100_most_common')
+         .to_excel('top_common_words.xlsx')
+         )
+        
+        tokens_updated = [
+            [word for word in token 
+             if word not in top_common_word and word in top_10k_word] 
+            for token in tokens_updated
+            ]
+        
+        tokens_updated = [token for token in tokens_updated if len(token)>=1]
+        
+        
+        dictionary = corpora.Dictionary(tokens_updated)
+        
+        corpus = [dictionary.doc2bow(text) for text in tokens_updated]
+        
+        num_topics = 15
+        
+        lda_model = LdaModel(
+            corpus=corpus,
+            id2word=dictionary,
+            num_topics=num_topics,
+            passes=30,
+            random_state=42
+            )
+        
+        topics = lda_model.print_topics(num_topics=num_topics, num_words=20)
+        
+        topics_dict = {"top_weighted_words": []}
+        
+        for topic in topics:
+            topics_dict["top_weighted_words"].append(
+                ", ".join(topic[1].split('"')[1::2])
+                )
+            
+        df_topics = pd.DataFrame(topics_dict)
+        
+        doc_topics = [lda_model.get_document_topics(bow) for bow in corpus]
+        
+        topic_interest = dict(zip(range(num_topics), [0]*num_topics))
+        
+        for doct in doc_topics:
+            for i, prob in doct:
+                topic_interest[i] += prob
+        
+        topic_interest = pd.DataFrame(
+            {"prob": topic_interest.values()},
+            index=topic_interest.keys()
+            )
+        
+        topic_interest = topic_interest/ len(self.load_documents())
+        
+        result = topic_interest.join(df_topics)
+        
+        result.to_csv('database/topic_allocation.csv', index=False)
+        
+        return print("Succesfully allocating words!")
+    
+    
+    
+        
+    
+    
     
     
     
--- main.py
+++ main.py
@@ -1,19 +1,85 @@
 #%% LIB
 from data import URLCrawl, DataRepository
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from datetime import datetime
+pd.set_option('display.max_columns', None)
 
 uc = URLCrawl()
 repo = DataRepository()
 
-#%% CRAWL URLS
+# df = repo.load_documents()
+# df.head()
+
+#%% CRAWL
 # url_list = [f"https://dantri.com.vn/tim-kiem/gi%C3%A1+v%C3%A0ng.htm?pi={index}"
 #             for index in range(1, 31)]
 
 # df = uc.get_url_collection(url_list)
 # df.to_csv('database/url_collection.csv', encoding='utf-8-sig')
 
-#%% CRAWL CONTENT
-df = repo.load_documents()
+#%% SENTIMENT TAGGING
+# repo.classify_gpt('database/content_wt_sentiment.csv')
+
+df = repo.load_setiment_docs()
 df.head()
 
-repo.export_ngram_list(num_gram=2, num_top=20)
+#%% EDA
+    #%% 1. What are the most common words in the content?
+repo.export_ngram_list(num_gram=15, num_top=20)
+
+    #%% 2. How frequent do they talk about the topic? Guess peak at new year
+df['month'] = pd.to_datetime(df['time']).dt.to_period("M")
+df['month'] = df['month'].dt.to_timestamp()
+df_frequency = df.groupby('month').size().rename('count').reset_index()
+df_frequency['month_str'] = df_frequency['month'].dt.strftime('%Y-%m')
+
+plt.figure(figsize=(9, 6))
+sns.barplot(
+    data=df_frequency,
+    # data=df_frequency[df_frequency['month']>pd.Timestamp(2024,1,1)], 
+    x='month_str', 
+    y='count'
+    )
+x_ticks = df_frequency['month_str'].to_list()
+plt.xticks(ticks=range(0, len(x_ticks), 6), 
+           label=x_ticks[::6],
+           rotation=45
+           )
+plt.show()
+
+    #%% 3. Sentiment analysis
+df[df['sentiment']=='Yếu tố thị trường tài chính']['content'].iloc[0]
+
+df['sentiment'].value_counts()
+
+
+    #%% 4. Latent Dirichlet Allocation
+
+repo.grouping_lda()
+
+df_lda = pd.read_csv('database/topic_allocation.csv')
+
+df_lda['topic_words'] = df_lda['top_weighted_words'].apply(
+    lambda x: ", ".join(x.split(',')[:4])
+    )
+
+df_lda = df_lda.sort_values('prob', ascending=False)
+
+df_lda = df_lda.reset_index()
+
+df_lda['topic_words'] = df_lda.index.astype(str) + '. ' + df_lda['topic_words']
+ 
+plt.figure(figsize=(6, 8), dpi=200)
+sns.barplot(
+    x=df_lda['prob']*100,
+    y=df_lda['topic_words'],
+    orient='y'
+    )
+plt.xlabel('Average proportion of topics [%]')
+plt.ylabel('Four most weighted words of the group')
+plt.show()
+
+
+
